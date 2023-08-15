d42568db10d06ac597d21a9ab36c627e48b3feb1
URTAAV <urieltapiaavalos@gmail.com>
2023-08-15 16:48:27 -0600

    dockerized app

---
+++ src/test/java/com/microservices/dockerized/postgresql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.dockerized.postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
